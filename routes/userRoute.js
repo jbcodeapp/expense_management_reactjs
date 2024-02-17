118acaec522fa6f3760b461c676876dddf4fbeb4
vishwa satasiya <vishwasatasiya@gmail.com>
2024-02-17 14:08:21 +0530

    initial

---
+++ routes/userRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { loginController, registerController } = require('../controllers/userController')
+
+//route object
+const route = express.Router()
+
+//routers
+//POST || LOGIN USER
+router.post('/login', loginController )
+
+//POST || REGISTER USER
+router.post('/register', registerController )
+
+
+
+module.exports = router
